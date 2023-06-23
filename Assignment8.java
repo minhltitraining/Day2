369e2b33df34918f5f7a636890027f69d2b684c4
minhltitraining <137228447+minhltitraining@users.noreply.github.com>
2023-06-23 14:22:41 +0000

    Update Assignment8.java

--- Assignment8.java
+++ Assignment8.java
@@ -1,37 +1,24 @@
 public class class1 {
-	int i = 22;
-	int j = 14000;
-	boolean k;
-	boolean l;
 	
 	public static void main(String[] args) {
-		i = 22;
-		j = 14000;
+		int i = 22;
+		int j = 14000;
 		class1 class1 = new class1();
-		k = class1.Method1(i);
-		l = class1.Method2(j);
+		boolean k = class1.Method1(i);
+		boolean l = class1.Method2(j);
 		
-		if (k == true && l == true) {
+		if (k && l) {
 			System.out.println("Is a new employee");
 		} else {
 			System.out.println("Is not a new employee");
 		}
 	}
 	
-	
 	public boolean Method1(int i) {
-		if (i > 20 && i < 30) {
-			return true;
-		} else {
-			return false;
-		}
+		return (i > 20 && i < 30);
 	}
 	
 	public boolean Method2 (int j) {
-		if  (j >=14000 && j < 20000) {
-			return true;
-		} else  {
-			return false;
-		}
+		return (j >=14000 && j < 20000);
 	}
 }
