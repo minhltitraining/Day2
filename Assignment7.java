f61571d7d152d8327fc81f51fca6338cf3795f2a
minhltitraining <137228447+minhltitraining@users.noreply.github.com>
2023-06-23 14:14:18 +0000

    Update Assignment7.java

--- Assignment7.java
+++ Assignment7.java
@@ -1,15 +1,16 @@
 public class passByVal {
 	public static void main(String[] args) {
-		int sId = 25;
+		Student s = new Student();
 		passByVal val = new passByVal();
-		System.out.println(sId);
-		val.passTheValueMethod(sId);
-		System.out.println("The sId are" + sId);
+		s.setStudentId(10);
+		System.out.println(s.getStudentId());
+		val.passTheValueMethod(s);
+		System.out.println("The sId are" + s.getStudentId());
 	}
 
-	private void passTheValueMethod(int sId) {
+	private void passTheValueMethod(Student s) {
 		// TODO Auto-generated method stub
-		sId = 10;
-		System.out.println("The sId are" + sId);
+		s.setStudentId(25);
+		System.out.println("The sId are" + s.getStudentId());
 	}
 }
